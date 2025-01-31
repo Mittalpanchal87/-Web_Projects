45173d59c2b71eb83159ce73fbdd5c6826068754
Mittalpanchal87 <mittelpanchal97@gmail.com>
2025-01-31 12:05:24 -0500

    Add files via upload

---
+++ cisar-project/cisar-project-2/js/custom_script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	new WOW().init();
+   
+   
+	$('#review_carousel').owlCarousel({
+	     margin:30,
+	     loop:true,
+		 slideSpeed : 300,
+		paginationSpeed : 400,
+		autoplay:true,
+		nav:false,
+		pagination:false,  dots: true,
+		navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:1
+	        },
+	        700:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        },
+	        1100:{
+	            items:1
+	        }
+	    }
+	}); 
+	 
+});
